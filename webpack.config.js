67261c497451b2d49fd52ca124c95254c2dfa079
laola1technik <laola1technik@users.noreply.github.com>
2017-11-20 14:00:07 +0100

    Adds Images and HTML to Project.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack'),
     ExtractTextPlugin = require("extract-text-webpack-plugin"),
     isProduction = process.env.NODE_ENV === "production";
 
-const extractLess = new ExtractTextPlugin('../../assets/css/[name].css');
+const extractLess = new ExtractTextPlugin('css/[name].css');
 
 module.exports = {
     context: __dirname,
@@ -17,27 +17,27 @@ module.exports = {
         extractLess
     ],
     entry: {
-        "layout_37": [
-            path.resolve(__dirname, "src/js/ticker.js"),
-            path.resolve(__dirname, "src/less/ticker.less")
+        "olympiaTicker": [
+            path.resolve(__dirname, "src/js/olympiaTicker.js"),
+            path.resolve(__dirname, "src/css/olympiaTicker.less")
         ]
     },
     output: {
-        path: path.resolve(__dirname, "public/assets/js/build/"),
-        filename: "[name].js"
+        path: path.resolve(__dirname, "public/assets/"),
+        filename: "js/[name].js"
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['env']
                 }
             },
             {
-                test: /\.less$/,
+                test: /\.(less|css)$/,
                 use: extractLess.extract({
                     use: [{
                         loader: "css-loader",
@@ -49,6 +49,38 @@ module.exports = {
                     }],
                     fallback: "style-loader"
                 })
+            },{
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&publicPath=../&name=./fonts/[hash].[ext]',
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'url-loader?limit=10000&publicPath=../&name=./img/generated/[hash].[ext]',
+                    {
+                        loader: 'img-loader?publicPath=../&name=./img/generated/[hash].[ext]',
+                        options: {
+                            enabled: isProduction,
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                arithmetic: false
+                            },
+                            optipng: false,
+                            pngquant: {
+                                floyd: 0.5,
+                                speed: 2
+                            },
+                            svgo: {
+                                plugins: [
+                                    { removeTitle: true },
+                                    { convertPathData: false }
+                                ]
+                            }
+                        }
+                    }
+                ]
             }
         ]
     }
