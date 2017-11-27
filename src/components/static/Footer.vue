2eebae0998fb807f242e4cf1bee86d0fda6ce2cc
Laola1 Technik <technik@laola1.at>
2017-11-27 14:04:58 +0100

    Setup Vue Cli Project.

---
+++ src/components/static/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="footer">
+        <span>produced by</span><a href="http://www.laola1.at" target="_blank"><img src="assets/img/l1logosmall.png"></a>
+    </div>
+</template>
